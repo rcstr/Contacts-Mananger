61ecadde750e734cce607168cb938980638b630a
Rommel Castro A <rommelxcastro@gmail.com>
2012-03-19 14:37:47 -0600

    first commit

---
+++ contacts-manager.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Plugin Name: Contacts Manager
+Description: Plugin creations for dreamstaffing
+Version: 1.0
+Author: Rommel Castro Alonzo
+Author URI: http://www.facebook.com/xrommelxcastrox
+*/
