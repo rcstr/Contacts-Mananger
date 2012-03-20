71ad4368af6a2e063e825f00e0429db9b1646472
Rommel Castro A <rommelxcastro@gmail.com>
2012-03-19 22:43:40 -0600

    edits on metabox, added functions for saving

--- contacts-manager.php
+++ contacts-manager.php
@@ -10,9 +10,34 @@
 
 class Contacts_Manager {
 
+    private $fields;
+
     public function __construct() {
         add_action('init', array($this, 'create_contact_type'));
-        add_action("add_meta_boxes", array($this, "contact_details_box"));
+        add_action('add_meta_boxes', array($this, 'contact_details_box'));
+        add_action('save_post', array($this, 'contact_details_save'));
+
+        //  Set up the fields
+        $this->fields = array(
+            'name' => array(
+                'title' => 'name',
+                'field_text' => 'Name'
+            ),
+            'last_name' => array(
+                'title' => 'last_name',
+                'field_text' => 'Last Name'
+            ),
+            'email' => array(
+                'title' => 'email',
+                'field_text' => 'Email',
+                'type' => 'email'
+            ),
+            'birthday' => array(
+                'title' => 'birthday',
+                'field_text' => 'Birthday',
+                'type' => 'date'
+            )
+        );
     }
 
     public function create_contact_type() {
@@ -49,26 +74,48 @@ class Contacts_Manager {
     }
 
     public function contact_details_box() {
-        add_meta_box("contact-details-box", "Contact Details", array($this, "contact_details_fields"), "contact", "normal", "high");
+        add_meta_box("contact-details-box", "Contact Details", array($this, "contact_details_fields_html"), "contact", "normal", "high");
     }
 
-    public function contact_details_fields() {
-        echo $this->contact_details_fields_html('name', 'Name');
-        echo $this->contact_details_fields_html('last-name', 'Last Name');
-        echo $this->contact_details_fields_html('email', 'Email');
-        echo $this->contact_details_fields_html('birthday', 'Birthday');
-    }
-
-    protected function contact_details_fields_html($title, $field_text) {
+    public function contact_details_fields_html() {
         global $post;
+        $html = '';
         $values = get_post_custom($post->ID);
-        $value = isset($value['contact_'.$title.'_field']) ? $values['contact_'.$title.'_field'];
+        foreach ($this->fields as $field) {
+            $field['value'] = isset($values['contact_' . $field['title'] . '_field']) ? $values['contact_' . $field['title'] . '_field'] : '';
+            $html .= '<label for="c-">' . $field['field_text'] . '</label>';
+            $html .= '<input type="text" name="contact_' . $field['title'] . '_field" id="contact_' . $field['title'] . '_field" value="' . $field['value'][0] . '" />';
+            $html .= '<br/>';
+        }
+        wp_nonce_field('contact-details-nonce', 'contact-details-nonce');
 
-        $html = '<label for="contact_'.$title.'_field">'.$field_text.'</label>';
-        $html .= '<input type="text" name="contact_'.$title.'_field" id="contact_'.$title.'_field" />';
-        $html .= '<br/>';
-        return $html;
+        echo $html;
     }
+
+    public function contact_details_save($post_id) {
+        // Bail if we're doing an auto save  
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+
+        // if our nonce isn't there, or we can't verify it, bail 
+        if (!isset($_POST['contact-details-nonce']) || !wp_verify_nonce($_POST['contact-details-nonce'], 'contact-details-nonce')) {
+            return;
+        }
+
+        // if our current user can't edit this post, bail  
+        if (!current_user_can('edit_post')) {
+            return;
+        }
+
+        //  Now we can actually save the data
+        foreach($this->fields as $field) {
+            if($_POST['contact_' . $field['title'] . '_field']) {
+                update_post_meta($post_id, 'contact_' . $field['title'] . '_field', $_POST['contact_' . $field['title'] . '_field']);
+            }
+        }
+    }
+
 }
 
 $contacts_Manager = new Contacts_Manager();
